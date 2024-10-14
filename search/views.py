96b22c1b59fc6be68967d1c8af24a9c94ea19c03
kingodjerry <ws.yjkim@gmail.com>
2024-10-15 08:57:10 +0900

    link css

--- search/views.py
+++ search/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'search/index.html')
+    content = {
+        'css_file': 'index.css'
+    }
+    return render(request, 'search/index.html', content)
